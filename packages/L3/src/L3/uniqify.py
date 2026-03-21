7f6fadf2d2655e1b6a56fb95f78be5bf0da1480a
Max De Jesus <me@maxdejesus.com>
2026-03-20 20:25:05 -0400

    test/implementation

--- packages/L3/src/L3/uniqify.py
+++ packages/L3/src/L3/uniqify.py
@@ -1,87 +1,113 @@
-# from collections.abc import Callable, Mapping
-# from functools import partial
+from collections.abc import Callable, Mapping
+from functools import partial
 
-# from util.sequential_name_generator import SequentialNameGenerator
+from util.sequential_name_generator import SequentialNameGenerator
 
-# from .syntax import (
-#     Abstract,
-#     Allocate,
-#     Apply,
-#     Begin,
-#     Branch,
-#     Immediate,
-#     Let,
-#     LetRec,
-#     Load,
-#     Primitive,
-#     Program,
-#     Reference,
-#     Store,
-#     Term,
-# )
+from .syntax import (
+    Abstract,
+    Allocate,
+    Apply,
+    Begin,
+    Branch,
+    Immediate,
+    Let,
+    LetRec,
+    Load,
+    Primitive,
+    Program,
+    Reference,
+    Store,
+    Term,
+)
 
-# type Context = Mapping[str, str]
-
-
-# def uniqify_term(
-#     term: Term,
-#     context: Context,
-#     fresh: Callable[[str], str],
-# ) -> Term:
-#     _term = partial(uniqify_term, context=context, fresh=fresh)
-
-#     match term:
-#         case Let(bindings=bindings, body=body):
-#             pass
-
-#         case LetRec(bindings=bindings, body=body):
-#             pass
-
-#         case Reference(name=name):
-#             pass
-
-#         case Abstract(parameters=parameters, body=body):
-#             pass
-
-#         case Apply(target=target, arguments=arguments):
-#             pass
-
-#         case Immediate():
-#             pass
-
-#         case Primitive(operator=operator, left=left, right=right):
-#             pass
-
-#         case Branch(operator=operator, left=left, right=right, consequent=consequent, otherwise=otherwise):
-#             pass
-
-#         case Allocate():
-#             pass
-
-#         case Load(base=base, index=index):
-#             pass
-
-#         case Store(base=base, index=index, value=value):
-#             pass
-
-#         case Begin(effects=effects, value=value):  # pragma: no branch
-#             pass
+type Context = Mapping[str, str]
 
 
-# def uniqify_program(
-#     program: Program,
-# ) -> tuple[Callable[[str], str], Program]:
-#     fresh = SequentialNameGenerator()
+def uniqify_term(
+    term: Term,
+    context: Context,
+    fresh: Callable[[str], str],
+) -> Term:
+    _term = partial(uniqify_term, context=context, fresh=fresh)
 
-#     _term = partial(uniqify_term, fresh=fresh)
+    match term:
+        case Let(bindings=bindings, body=body):
+            new_bindings_rhs = [_term(rhs) for _, rhs in bindings]
+            local = {name: fresh(name) for name, _ in bindings}
+            new_context = {**context, **local}
+            return Let(
+                bindings=[(local[name], rhs) for (name, _), rhs in zip(bindings, new_bindings_rhs)],
+                body=uniqify_term(body, new_context, fresh),
+            )
 
-#     match program:
-#         case Program(parameters=parameters, body=body):  # pragma: no branch
-#             local = {parameter: fresh(parameter) for parameter in parameters}
-#             return (
-#                 fresh,
-#                 Program(
-#                     parameters=[local[parameter] for parameter in parameters],
-#                     body=_term(body, local),
-#                 ),
-#             )
+        case LetRec(bindings=bindings, body=body):
+            local = {name: fresh(name) for name, _ in bindings}
+            new_context = {**context, **local}
+            _rec = partial(uniqify_term, context=new_context, fresh=fresh)
+            return LetRec(
+                bindings=[(local[name], _rec(rhs)) for name, rhs in bindings],
+                body=_rec(body),
+            )
+
+        case Reference(name=name):
+            return Reference(name=context[name])
+
+        case Abstract(parameters=parameters, body=body):
+            local = {param: fresh(param) for param in parameters}
+            new_context = {**context, **local}
+            return Abstract(
+                parameters=[local[param] for param in parameters],
+                body=uniqify_term(body, new_context, fresh),
+            )
+
+        case Apply(target=target, arguments=arguments):
+            return Apply(
+                target=_term(target),
+                arguments=[_term(arg) for arg in arguments],
+            )
+
+        case Immediate():
+            return term
+
+        case Primitive(operator=operator, left=left, right=right):
+            return Primitive(operator=operator, left=_term(left), right=_term(right))
+
+        case Branch(operator=operator, left=left, right=right, consequent=consequent, otherwise=otherwise):
+            return Branch(
+                operator=operator,
+                left=_term(left),
+                right=_term(right),
+                consequent=_term(consequent),
+                otherwise=_term(otherwise),
+            )
+
+        case Allocate():
+            return term
+
+        case Load(base=base, index=index):
+            return Load(base=_term(base), index=index)
+
+        case Store(base=base, index=index, value=value):
+            return Store(base=_term(base), index=index, value=_term(value))
+
+        case Begin(effects=effects, value=value):  # pragma: no branch
+            return Begin(effects=[_term(e) for e in effects], value=_term(value))
+
+
+def uniqify_program(
+    program: Program,
+) -> tuple[Callable[[str], str], Program]:
+    fresh = SequentialNameGenerator()
+
+    _term = partial(uniqify_term, fresh=fresh)
+
+    match program:
+        case Program(parameters=parameters, body=body):  # pragma: no branch
+            local = {parameter: fresh(parameter) for parameter in parameters}
+            return (
+                fresh,
+                Program(
+                    parameters=[local[parameter] for parameter in parameters],
+                    body=_term(body, local),
+                ),
+            )
--- packages/L3/test/L3/test_uniqify.py
+++ packages/L3/test/L3/test_uniqify.py
@@ -1,65 +1,256 @@
-# import pytest
-# from L3.syntax import Apply, Immediate, Let, Reference
-# from L3.uniqify import Context, uniqify_term
-# from util.sequential_name_generator import SequentialNameGenerator
+from L3.syntax import (
+    Abstract,
+    Allocate,
+    Apply,
+    Begin,
+    Branch,
+    Immediate,
+    Let,
+    LetRec,
+    Load,
+    Primitive,
+    Reference,
+    Store,
+)
+from L3.uniqify import Context, uniqify_term
+from util.sequential_name_generator import SequentialNameGenerator
 
 
-# @pytest.mark.skip
-# def test_uniqify_term_reference():
-#     term = Reference(name="x")
+def test_uniqify_term_reference():
+    term = Reference(name="x")
 
-#     context: Context = {"x": "y"}
-#     fresh = SequentialNameGenerator()
-#     actual = uniqify_term(term, context, fresh=fresh)
+    context: Context = {"x": "y"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh=fresh)
 
-#     expected = Reference(name="y")
+    expected = Reference(name="y")
 
-#     assert actual == expected
-
-
-# @pytest.mark.skip
-# def test_uniqify_immediate():
-#     term = Immediate(value=42)
-
-#     context: Context = dict[str, str]()
-#     fresh = SequentialNameGenerator()
-#     actual = uniqify_term(term, context, fresh)
-
-#     expected = Immediate(value=42)
-
-#     assert actual == expected
+    assert actual == expected
 
 
-# @pytest.mark.skip
-# def test_uniqify_term_let():
-#     term = Let(
-#         bindings=[
-#             ("x", Immediate(value=1)),
-#             ("y", Reference(name="x")),
-#         ],
-#         body=Apply(
-#             target=Reference(name="x"),
-#             arguments=[
-#                 Reference(name="y"),
-#             ],
-#         ),
-#     )
+def test_uniqify_immediate():
+    term = Immediate(value=42)
 
-#     context: Context = {"x": "y"}
-#     fresh = SequentialNameGenerator()
-#     actual = uniqify_term(term, context, fresh)
+    context: Context = dict[str, str]()
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
 
-#     expected = Let(
-#         bindings=[
-#             ("x0", Immediate(value=1)),
-#             ("y0", Reference(name="y")),
-#         ],
-#         body=Apply(
-#             target=Reference(name="x0"),
-#             arguments=[
-#                 Reference(name="y0"),
-#             ],
-#         ),
-#     )
+    expected = Immediate(value=42)
 
-#     assert actual == expected
+    assert actual == expected
+
+
+def test_uniqify_term_let():
+    term = Let(
+        bindings=[
+            ("x", Immediate(value=1)),
+            ("y", Reference(name="x")),
+        ],
+        body=Apply(
+            target=Reference(name="x"),
+            arguments=[
+                Reference(name="y"),
+            ],
+        ),
+    )
+
+    context: Context = {"x": "y"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Let(
+        bindings=[
+            ("x0", Immediate(value=1)),
+            ("y0", Reference(name="y")),
+        ],
+        body=Apply(
+            target=Reference(name="x0"),
+            arguments=[
+                Reference(name="y0"),
+            ],
+        ),
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_letrec():
+    term = LetRec(
+        bindings=[
+            ("f", Abstract(parameters=["x"], body=Apply(target=Reference(name="g"), arguments=[Reference(name="x")]))),
+            ("g", Abstract(parameters=["x"], body=Apply(target=Reference(name="f"), arguments=[Reference(name="x")]))),
+        ],
+        body=Apply(target=Reference(name="f"), arguments=[Immediate(value=1)]),
+    )
+
+    context: Context = dict[str, str]()
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = LetRec(
+        bindings=[
+            (
+                "f0",
+                Abstract(parameters=["x0"], body=Apply(target=Reference(name="g0"), arguments=[Reference(name="x0")])),
+            ),
+            (
+                "g0",
+                Abstract(parameters=["x1"], body=Apply(target=Reference(name="f0"), arguments=[Reference(name="x1")])),
+            ),
+        ],
+        body=Apply(target=Reference(name="f0"), arguments=[Immediate(value=1)]),
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_abstract():
+    term = Abstract(
+        parameters=["x", "y"],
+        body=Primitive(operator="+", left=Reference(name="x"), right=Reference(name="y")),
+    )
+
+    context: Context = dict[str, str]()
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Abstract(
+        parameters=["x0", "y0"],
+        body=Primitive(operator="+", left=Reference(name="x0"), right=Reference(name="y0")),
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_apply():
+    term = Apply(
+        target=Reference(name="f"),
+        arguments=[Reference(name="x"), Reference(name="y")],
+    )
+
+    context: Context = {"f": "f0", "x": "x0", "y": "y0"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Apply(
+        target=Reference(name="f0"),
+        arguments=[Reference(name="x0"), Reference(name="y0")],
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_primitive():
+    term = Primitive(
+        operator="*",
+        left=Reference(name="x"),
+        right=Immediate(value=2),
+    )
+
+    context: Context = {"x": "x0"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Primitive(
+        operator="*",
+        left=Reference(name="x0"),
+        right=Immediate(value=2),
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_branch():
+    term = Branch(
+        operator="<",
+        left=Reference(name="x"),
+        right=Immediate(value=0),
+        consequent=Immediate(value=1),
+        otherwise=Reference(name="y"),
+    )
+
+    context: Context = {"x": "x0", "y": "y0"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Branch(
+        operator="<",
+        left=Reference(name="x0"),
+        right=Immediate(value=0),
+        consequent=Immediate(value=1),
+        otherwise=Reference(name="y0"),
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_allocate():
+    term = Allocate(count=3)
+
+    context: Context = dict[str, str]()
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Allocate(count=3)
+
+    assert actual == expected
+
+
+def test_uniqify_term_load():
+    term = Load(
+        base=Reference(name="arr"),
+        index=0,
+    )
+
+    context: Context = {"arr": "arr0"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Load(
+        base=Reference(name="arr0"),
+        index=0,
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_store():
+    term = Store(
+        base=Reference(name="arr"),
+        index=0,
+        value=Reference(name="v"),
+    )
+
+    context: Context = {"arr": "arr0", "v": "v0"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Store(
+        base=Reference(name="arr0"),
+        index=0,
+        value=Reference(name="v0"),
+    )
+
+    assert actual == expected
+
+
+def test_uniqify_term_begin():
+    term = Begin(
+        effects=[
+            Store(base=Reference(name="arr"), index=0, value=Immediate(value=99)),
+        ],
+        value=Reference(name="arr"),
+    )
+
+    context: Context = {"arr": "arr0"}
+    fresh = SequentialNameGenerator()
+    actual = uniqify_term(term, context, fresh)
+
+    expected = Begin(
+        effects=[
+            Store(base=Reference(name="arr0"), index=0, value=Immediate(value=99)),
+        ],
+        value=Reference(name="arr0"),
+    )
+
+    assert actual == expected
